11d7c81b69188677765b3ab8473fcfd2c050e488
Fauzan <fauzantakibang@gmail.com>
2024-11-14 21:17:54 +0800

    else if + operator logika

--- angkot5.js
+++ angkot5.js
@@ -6,10 +6,7 @@ for(var noAngkot = 1; noAngkot <= jmlhAngkot; noAngkot++) {
     if(noAngkot <= angkotBeroprasi) {
         console.log(`Angkot ${noAngkot} sedang beroprasi`)
     } 
-    else if(noAngkot === 8) {
-        console.log(`Angkot ${noAngkot} sedang lembur`)
-    }
-    else if (noAngkot === 10) {
+    else if(noAngkot === 8 || noAngkot === 10) {
         console.log(`Angkot ${noAngkot} sedang lembur`)
     }
     else {
