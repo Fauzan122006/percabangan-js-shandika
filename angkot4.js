40c4a399a862d7d3ea10eaa3e2b71217107e6f7a
Fauzan <fauzantakibang@gmail.com>
2024-11-14 21:02:30 +0800

    else-if

---
+++ angkot4.js
@@ -0,0 +1,15 @@
+var jmlhAngkot = 10
+var angkotBeroprasi = 6
+
+
+for(var noAngkot = 1; noAngkot <= jmlhAngkot; noAngkot++) {
+    if(noAngkot <= angkotBeroprasi) {
+        console.log(`Angkot ${noAngkot} sedang beroprasi`)
+    } 
+    else if(noAngkot === 8) {
+        console.log(`Angkot ${noAngkot} sedang lembur`)
+    }
+    else {
+        console.log(`Angkot ${noAngkot} tidak beroprasi`)
+    }
+}
